1af5e67366b487a07313a714e837c9842db8d91f
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2025-12-02 12:33:14 +0200

    1

---
+++ introduction to ETL Processes-exercise/config/settings.py
@@ -0,0 +1,11 @@
+AWS_ACCESS_KEY_ID = ""
+AWS_SECRET_ACCESS_KEY = ""
+
+BUCKET_NAME = "course-data-warehouse-viki"
+FOLDER ="ETLANDELT/"
+
+USER = "postgres"
+PASSWORD = ""
+HOST = "localhost"
+PORT = "5432"
+DATABASE = "etl_ex_load_data"
